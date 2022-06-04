d8e40d49263273df0f74db5d916027787bb86728
Iago Fabricio <iago.f10@hotmail.com>
2022-06-04 16:06:38 -0300

    Ajustes na publicação ampliada e foto ampliada.

--- jobbe-web/wwwroot/js/site.js
+++ jobbe-web/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿var RotaAPI = 'https://192.168.0.209:5001/'
-var RotaWeb = 'https://192.168.0.209:8081/'
+﻿var RotaAPI = 'https://192.168.15.103:5001/'
+var RotaWeb = 'https://192.168.15.103:8081/'
 //var RotaAPI = 'https://api.iago2204270158.bateaquihost.com.br/'
 //var RotaWeb = 'https://iago2204270158.bateaquihost.com.br/'
